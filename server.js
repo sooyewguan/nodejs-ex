a90274cfd1f648822dac5afe0bd780bc24af79f1
sooyewguan <sooyewguan@sooyewguans-MacBook-Pro.local>
2016-12-25 02:19:09 +0800

    socket.io

--- server.js
+++ server.js
@@ -64,38 +64,12 @@ var initDb = function(callback) {
   });
 };
 
+server.listen(port);
+
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+  res.render('index.html');
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
 
 // error handling
 app.use(function(err, req, res, next){
@@ -107,8 +81,8 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+//app.listen(port, ip);
+//console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
 
